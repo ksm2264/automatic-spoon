13f7d7f1cbd726b8de13d907ae23f639af4f68b0
karl <karl.muller@utexas.edu>
2021-03-25 18:31:02 -0500

    video strings

--- createVideoSegments.py
+++ createVideoSegments.py
@@ -7,4 +7,15 @@ Created on Thu Mar 25 18:13:28 2021
 """
 
 import numpy as np
+import pandas as pd
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
+tab = pd.read_csv('timestamps_seconds.csv').dropna()
+
+base_vid_path = '/media/karl/DATA/vid/'
+
+for subj in tab['Subject']:
+    
+    raw_vid_str = base_vid_path + subj +'.mp4'
+    
+    print(raw_vid_str)
